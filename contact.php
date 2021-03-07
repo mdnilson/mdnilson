b02ef6bc7378d01c0ce3cf748f937f0c5f392e58
Marek Dziezak <marekd5@hotmail.com>
2021-03-07 17:14:24 +0000

    small fixes

--- contact.php
+++ contact.php
@@ -7,6 +7,44 @@
     <title>Contact</title>
 </head>
 <body>
-    Już niedługo...
+<div class="container-fluid p-0">
+    <!-- NAVIGATION -->
+    <nav class="navbar navbar-dark navbar-expand-md bg-mdnilson">
+        <a class="navbar-brand" href="../index.html">MD</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.html">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CSS</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="pozycja.html">Pozycja elementów</a></li>
+                        <li><a class="dropdown-item" href="responsive_grid.html">Responsywny grid</a></li>
+                        <li>
+                            <hr class="dropdown-divider" </li>
+                        <li><a class="dropdown-item disabled" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../python.html">Python</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../php.html">PHP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="contact.php">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- MAIN -->
+    <h1>Już niedługo...</h1>
+</div>
 </body>
 </html>
